633bdca0233f4a3b670ae8fbee5ff66b10cadfac
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2020-08-08 23:06:20 +0800

    Updates

---
+++ precache-manifest.205aae861cdee2d37f343600c75d1b11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fc7df60b4bd3b9e3d72f34c897a4b8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6c31ebce72aaeb8eac8a",
+    "url": "/monsters-rolodex/static/css/main.c93f6d08.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c31ebce72aaeb8eac8a",
+    "url": "/monsters-rolodex/static/js/main.7ea45ec3.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
